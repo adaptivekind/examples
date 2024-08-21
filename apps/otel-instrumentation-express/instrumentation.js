8eae0c4862414cc5d42fdfd78f1800c99b5817be
Ian Homer <code@ianhomer.com>
2024-08-21 16:17:07 +0100

    Metrics exporter for express api

---
+++ apps/otel-instrumentation-express/instrumentation.js
@@ -0,0 +1,38 @@
+/*
+ * NodeJS auto instrumentation does not yet generate metrics.
+ *
+ * https://github.com/open-telemetry/opentelemetry.io/issues/4171
+ *
+ * ... hence why we need this instrumentation code, instead of driven by
+ * environment variables.
+ *
+ * Code with thanks from
+ *
+ * https://opentelemetry.io/docs/languages/js/getting-started/nodejs/
+ */
+
+const { NodeSDK } = require("@opentelemetry/sdk-node");
+const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+const {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} = require("@opentelemetry/sdk-metrics");
+const {
+  OTLPMetricExporter,
+} = require('@opentelemetry/exporter-metrics-otlp-http');
+
+const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter:
+      process.env.OTEL_METRICS_EXPORTER === "console"
+        ? new ConsoleMetricExporter()
+        : new OTLPMetricExporter(),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
